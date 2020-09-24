d030fb2d791738e37457859ac710164d646bfc01
Cecilia Dornelas <ceciliadornelas.pb@gmail.com>
2020-09-24 16:03:06 -0300

    tarefa

---
+++ exercicio/quest do livro/processo.php
@@ -0,0 +1,13 @@
+<?php
+    $nome = $_POST ['nome'];
+    $email = $_POST ['email'];
+    $tel = $_POST ['tel'];
+    $sobrenome = $_POST ['sobrenome'];
+
+    echo " Nome: ". $nome . $sobrenome ;
+    echo "<br>";
+    echo " E-mail: ". $email ;
+    echo "<br>";
+    echo " telefone: ". $tel ;
+?>
+<p><a href="cadastro.html">>>Voltar<<</a></p>
