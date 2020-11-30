1af823c995954a60ef334529c42206a08d13a98b
CeciliaDornelas <ceciliadornelas.pb@gmail.com>
2020-11-30 00:30:45 -0300

    cadastro no banco de dados

--- form php com BD/alteracao.php
+++ form php com BD/alteracao.php
@@ -19,16 +19,30 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="estilos/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="estilos/style.css">
+    <link rel="stylesheet" href="estilos/cadastro.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <title>tabela de users</title>
 </head>
 <body>
-    <form action="update.php" method="POST" class="alteracao">
-        <input type="hidden" name="cod" value="<?php echo $cod;?>">
-        <input type="text" name="nome" value="<?php echo $nome;?>">
-        <input type="email" name="email" value="<?php echo $email;?>">
-        <input type="tel" name="tel" value="<?php echo $tel;?>">
-        <input type="submit" value="salvar">
-    </form>
+    <div class="alinhar">
+        <form action="update.php" method="POST" class="alteracao">
+            <h3>Editar:</h3>
+            <div class="ajustes">
+                <input type="hidden" name="cod" value="<?php echo $cod;?>">
+                <p>nome:<input type="text" class="inputs" name="nome" value="<?php echo $nome;?>"></p>
+            </div>
+            <div class="ajustes">
+                <p>email:<input type="email" class="inputs" name="email" value="<?php echo $email;?>"></p>
+            </div>
+            <div class="ajustes">
+                <p>telefone:<input type="tel" class="inputs" name="tel" value="<?php echo $tel;?>"></p>
+            </div>
+                <input type="submit" class="btn btn-primary" value="salvar">
+            
+        </form>
+    </div>
 </body>
 </html>
--- form php com BD/tabela.php
+++ form php com BD/tabela.php
@@ -8,24 +8,32 @@
 <!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="estilos/style.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>tabela de users</title>
-    <link rel="stylesheet" href="estilos/style.css">
+    <title>Tabela de Users</title>
+
 </head>
 <body>
-    <table>
-        <tr class="titulos">
-            <td>código</td>
-            <td>nome do usuário</td>
-            <td>email</td>
-            <td>telefone</td>
-            <td colspan="2">mudanças</td>
-        </tr>
+    <h4>Usuários Cadastrados</h4>
+          
+    <table class="table" style=" position: absolute; top: 20%; font-size: 20px; color: rgb(49, 48, 48); border-radius: 5px; text-align: center;" >
         
+        <tr class="titulos alert alert-secondary">
+            <th scope="col">Código</th>
+            <th scope="col">Nome do Usuário</th>
+            <th scope="col">Email</th>
+            <th scope="col">Telefone</th>
+            <th scope="col" colspan="2">Ações</th>
+        </tr>
 <?php
     $result = $conexao->query($select);
-    while($tbl = mysqli_fetch_array($result)){
+    while($tbl = mysqli_fetch_array($result)):
         $cod = $tbl['codigo'];
         $nome = $tbl['nome'];
         $email = $tbl['email'];
@@ -36,13 +44,13 @@
         echo "<td>$nome</td>";
         echo "<td>$email</td>";
         echo "<td>$tel</td>";
-        echo "<td><a href='alteracao.php?id=$cod' class='alt'>editar</a></td>";
-        echo "<td><a href='exclusao.php?id=$cod' class='alt'>remover</a></td>";
-        echo"</tr>";
-    }
 ?>
-    <tr class="cad">
-        <td><a href="cadastro.html">Cadastro</a></td></td>
+    <td> <icon><a href='alteracao.php?id=<?php echo $cod; ?>' class='alt btn btn-primary'>editar</a></td></icon>
+    <td> <icon><a href='exclusao.php?id=<?php echo $cod; ?>' class='alt btn btn-danger'>remover</a></td></button>
     </tr>
+<?php endwhile; ?>
+    <div class="div-cad">
+        <button type="button" class="btn btn-primary cad-usuario"><a class="cad-link" href="cadastro.html" >Cadastrar Novo Usuário</a></button>
+    </div>
 </body>
 </html>
